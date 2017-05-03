4cbc5b090e39ce03036c64ac0efd7e32be51e94b
pham-thien <pham.thien@outlook.com>
2017-05-03 17:35:24 -0400

    edit

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const chaiHttp = require('chai-http');
 const faker = require('faker');
 const mongoose = require('mongoose');
 console.log('\x1Bc');
-const {BlogPost} = require('../models');
+var {BlogPost} = require('../models');
 const {app, runServer, closeServer} = require('../server');
 
 
