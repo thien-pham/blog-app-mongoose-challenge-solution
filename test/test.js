7a31b59d6f4de236a1327c0a930e0fbe3592da76
Zach Williams <saumanahaii@gmail.com>
2017-05-03 16:08:38 -0400

    added post and put tests

--- test/test.js
+++ test/test.js
@@ -10,29 +10,34 @@ const {app, runServer, closeServer} = require('../server');
 chai.should();
 chai.use(chaiHttp);
 
+function generatePost(){
+  return {
+     'title': faker.lorem.sentence(),
+     'content': faker.lorem.paragraph(),
+     'author': {
+        'firstName': faker.name.firstName(),
+        'lastName': faker.name.lastName()
+      }
+  }
+}
+
 function seedPostData(){
   const seedData = [];
-
   for(let i = 0; i < 4; i++){
-    const generatePost = {
-       'title': faker.lorem.sentence(),
-       'content': faker.lorem.paragraph(),
-       'author': {
-          'firstName': faker.name.firstName(),
-          'lastName': faker.name.lastName()
-        }
-     };
-    seedData.push(generatePost);
-  }
-  return BlogPost.insertMany(seedData);    
+    seedData.push(generatePost());
+  };
+  return BlogPost.insertMany(seedData);
 }
+
+
+
 
 function tearDownDb() {
   return BlogPost.db.dropDatabase();
 }
 
 describe('Blog server startup',() => {
-  
+
   before(() => {
     runServer();
   });
@@ -54,7 +59,7 @@ describe('Blog server startup',() => {
       return chai.request(app)
       .get('/posts')
       .then((result)=>{
-        console.dir(result.body);
+        //console.dir(result.body);
         result.should.have.status(200);
         result.should.be.json;
         result.body.should.be.a('array');
@@ -62,4 +67,39 @@ describe('Blog server startup',() => {
       });
     });
   });
+  describe('\nTesting POST Endpoint',() => {
+    it('\nShould return 201 for create',() => {
+      return chai.request(app)
+      .post('/posts')
+      .send(generatePost())
+      .then((result) => {
+          console.log(result.body);
+          result.should.have.status(201);
+          result.should.be.json;
+          result.body.should.be.a('object');
+          result.body.should.have.ownProperty('author');
+      })
+    })
+  })
+  describe('\nTesting Put',() => {
+    it('\nShould return 201',() => {
+      const updateData = {
+        "title"   : "!!!!!FOO!!!!!",
+        "content" : "iiiiiBARiiiii"
+      }
+      return BlogPost
+      .findOne()
+      .then(function(postData) {
+        updateData.id = postData.id;
+        return chai.request(app).put(`/posts/${postData.id}`)
+        .send(updateData);
+      }).then(function(postData){
+        postData.should.have.status(201);
+        postData.should.be.json;
+        postData.body.should.be.a('object');
+        postData.body.should.include.keys('id','author','content');
+      })
+    })
+  })
+
 });
